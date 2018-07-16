f21641268e093aab0311743c2f3cfb52375c485f
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-16 16:18:54 +0700

    Getting row values

--- eureka_test/MyFormViewController.swift
+++ eureka_test/MyFormViewController.swift
@@ -18,10 +18,12 @@ class MyFormViewController: FormViewController {
       <<< TextRow() { row in
         row.title = "Text Row"
         row.placeholder = "Enter text here"
+        row.tag = "nameText"
       }
       <<< PhoneRow() {
         $0.title = "Phone Row"
         $0.placeholder = "Add number here"
+        $0.tag = "phoneText"
       }
     +++ Section("Section 2")
       <<< LabelRow() {
@@ -67,6 +69,15 @@ class MyFormViewController: FormViewController {
       <<< DateRow() {
         $0.title = "Data Row"
         $0.value = Date(timeIntervalSinceReferenceDate: 0)
+        $0.tag = "dateText"
+      }
+    
+    +++ Section()
+      <<< ButtonRow() { (row: ButtonRow) -> Void in
+        row.title = "Get Values"
+      }
+      .onCellSelection { [weak self] (cell, row) in
+        self?.testGetValues()
       }
     
     // Enables the navigation accessory and stops navigation when a disabled row is encountered
@@ -75,5 +86,12 @@ class MyFormViewController: FormViewController {
     animateScroll = true
     // Leaves 60pt of space between the keyboard and the highlighted row after scrolling to an off screen row
     rowKeyboardSpacing = 60
+  }
+  
+  private func testGetValues() {
+    // Get the value of all rows which have a Tag assigned
+    // The dictionary contains the 'rowTag':value pairs.
+    let valuesDictionary = form.values()
+    print(valuesDictionary)
   }
 }
